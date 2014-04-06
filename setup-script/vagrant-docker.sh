dfedb7a6d559ce3832d6b544042a068d36f958e6
Koichi Wada <kw.nachos@gmail.com>
2014-04-06 23:43:52 +0900

    Setting up boot2docker box by vagrant init.

--- setup-script/vagrant-docker.sh
+++ setup-script/vagrant-docker.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+BOOT2DOCKER_BOX_VERSION="0.5.4-1"
+BOOT2DOCKER_BOX_URL="https://github.com/mitchellh/boot2docker-vagrant-box/releases/download/v${BOOT2DOCKER_BOX_VERSION}/boot2docker_virtualbox.box"
+
 SETUP_SCRIPT_DIR=$(dirname $0)
-SRC_DIR=$SETUP_SCRIPT_DIR/../src
+SRC_DIR=$(cd $SETUP_SCRIPT_DIR/../src && pwd)
 
 VAGRANT_ROOT_DIR=$HOME/Vagrant
 VAGRANT_DIR=$VAGRANT_ROOT_DIR/docker
@@ -16,12 +19,11 @@ if [ ! -d $VAGRANT_DIR ]; then
     mkdir $VAGRANT_DIR
 fi
 
-echo "cp $SRC_DIR/Vagrantfile.boot2docker $VAGRANT_DIR/Vagrantfile"
-cp $SRC_DIR/Vagrantfile.boot2docker $VAGRANT_DIR/Vagrantfile
+# vagrant init
+cd $VAGRANT_DIR
+vagrant init boot2docker $BOOT2DOCKER_BOX_URL
 
-echo "cp $SRC_DIR/vagrant-docker $HOME/bin/."
-echo "chmod +x $HOME/bin/vagrant-docker"
-
+# vagrant-docker
 cp $SRC_DIR/vagrant-docker $HOME/bin/.
 chmod +x $HOME/bin/vagrant-docker
 
