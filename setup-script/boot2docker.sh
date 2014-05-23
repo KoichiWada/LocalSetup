5696aac28e9710ecf4a10801bceb9b786396d99a
Koichi Wada <kw.nachos@gmail.com>
2014-05-24 03:06:15 +0900

    boot2docker initialization at installation.

--- setup-script/boot2docker.sh
+++ setup-script/boot2docker.sh
@@ -8,12 +8,16 @@ INSTALL_DIR=$HOME/bin
 TARGET_NAME=boot2docker
 DEST_PATH=$INSTALL_DIR/$TARGET_NAME
 
+SRC_URL=https://raw.githubusercontent.com/boot2docker/boot2docker/master/boot2docker
+
 if [ -f $DEST_PATH ]; then
     echo "'$DEST_PATH' exists."
 else
-    curl -fsSL https://raw.github.com/boot2docker/boot2docker/master/boot2docker > $DEST_PATH
+    curl -fsSL $SRC_URL > $DEST_PATH
     chmod +x $DEST_PATH
     echo "'$DEST_PATH' has been installed."
+    $DEST_PATH download
+    $DEST_PATH init
+    $SETUP_SCRIPT_DIR/virtualbox-portforwarding.sh 49000 49900
 fi
 
-$SETUP_SCRIPT_DIR/virtualbox-portforwarding.sh 49000 49900
