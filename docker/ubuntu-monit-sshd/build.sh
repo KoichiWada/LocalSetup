720813832a18d981029a12b9eb2472a2060207d5
Koichi Wada <kw.nachos@gmail.com>
2014-05-26 03:12:16 +0900

    Timezone, monit, rsyslog, cron settings.

--- docker/ubuntu-monit-sshd/build.sh
+++ docker/ubuntu-monit-sshd/build.sh
@@ -2,4 +2,9 @@
 
 cd $(dirname $0)
 cp $HOME/.ssh/id_rsa.pub ./key.pub
+
 docker build -t wada/ubuntu-monit-sshd .
+
+docker rm $(docker ps -aq)
+docker rmi $(docker images | awk '/^<none>/ { print $3 }')
+
