1ea1da5a866e99f2bd1efdb85d8766ad0045257c
Koichi Wada <kw.nachos@gmail.com>
2014-10-14 00:26:20 +0900

    Shell script instead of brew bundle.

--- setup-script/brew-bundle.sh
+++ setup-script/brew-bundle.sh
@@ -3,6 +3,53 @@
 SETUP_SCRIPT_DIR=$(dirname $0)
 SRC_DIR=$SETUP_SCRIPT_DIR/../src
 
-cd $SRC_DIR
+TAPS=(
+    homebrew/binary
+    homebrew/versions
+    phinze/homebrew-cask
+)
+
+FORMULAS=(
+    brew-cask
+    jq
+    jenkins
+    #composer
+    ## Graphviz
+    gts
+    graphviz
+)
+
+CASKS=(
+# Cask Packages
+    iterm2
+    alfred
+    evernote
+    dropbox
+    skype
+    mysqlworkbench
+    sequel-pro
+    firefox
+    google-chrome
+    virtualbox
+    vagrant
+    eclipse-ide
+)
+
 brew update
-brew bundle
+brew upgrade
+
+# taps
+for tap in "${TAPS[@]}"; do
+    brew tap $tap
+done
+
+for formula in "${FORMULAS[@]}"; do
+    brew install $formula
+done
+
+for cask in "${CASKS[@]}"; do
+    brew cask install $cask
+done
+
+brew cleanup
+brew doctor
