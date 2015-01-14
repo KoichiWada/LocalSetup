fbb38d400d9e1baa108b8a1d9df4b1749a4e70d4
Koichi Wada <kw.nachos@gmail.com>
2015-01-14 18:04:05 +0900

    Updated git-completion installation script.

--- setup-script/git-completion.sh
+++ setup-script/git-completion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
 
 if [ $(grep -c git-completion.bash ~/.bashrc) -eq 0 ]; then
     cat $(dirname $0)/../src/git-completion >> ~/.bashrc
